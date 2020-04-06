87abe679c7340993205a07be5c7d327fd9b2fec5
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-04-06 17:33:58 +0700

    2-Fetching Movie Data

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '5c7875e9',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+
+};
+
+fetchData();
