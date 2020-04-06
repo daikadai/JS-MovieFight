8d77f7fbb8ad542f74fd836d229552eab896ebdb
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-04-06 17:42:15 +0700

    3-Fetching a single movie

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '5c7875e9',
-      s: 'avengers'
+      i: 'tt0848228'
     }
   });
 
