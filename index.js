cb00bac8941b0d083d5cf3e9b3ce68c696cb8bee
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-04-20 15:12:04 +0700

    11. Handling Broken Image, Automatically closing the dropdown

--- index.js
+++ index.js
@@ -30,12 +30,14 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
+  resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
   for (let movie of movies) {
     const option = document.createElement('a');
+    const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster;
     option.classList.add('dropdown-item');
     option.innerHTML = `
-      <img src="${movie.Poster}" />
+      <img src="${imgSRC}" />
       ${movie.Title}
     `
     resultsWrapper.appendChild(option);
@@ -43,3 +45,9 @@ const onInput = async (e) => {
 };
 
 input.addEventListener('input', debounce(onInput, 1000));
+
+document.addEventListener('click', e => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove('is-active');
+  }
+})
