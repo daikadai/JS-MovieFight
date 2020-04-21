6c571cee3ccde4a97233292e9c1b30f6e544c052
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-04-21 13:17:12 +0700

    16. Display Multiple Autocomplete

--- index.js
+++ index.js
@@ -13,53 +13,17 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <labe><b>Search for a movide</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
- `
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
+createAutoComplete({
+  root: document.querySelector('.autocomplete')
+});
 
+createAutoComplete({
+  root: document.querySelector('.autocomplete-two')
+});
 
-const onInput = async (e) => {
-  const movies = await fetchData(e.target.value);
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster;
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-      <img src="${imgSRC}" />
-      ${movie.Title}
-    `
-    option.addEventListener('click', e => {
-      dropdown.classList.remove('is-active');
-      input.value = movie.Title;
-      onMovieSelect(movie)
-    })
-    resultsWrapper.appendChild(option);
-  }
-};
-
-input.addEventListener('input', debounce(onInput, 1000));
-
-document.addEventListener('click', e => {
-  if (!root.contains(e.target)) {
-    dropdown.classList.remove('is-active');
-  }
-})
+createAutoComplete({
+  root: document.querySelector('.autocomplete-three')
+});
 
 const onMovieSelect = async (movie) => {
   const response = await axios.get('http://www.omdbapi.com/', {
