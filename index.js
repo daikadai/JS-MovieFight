08004816c4ff3864456e412baac6b9b9415a1747
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-04-22 11:43:45 +0700

    19. Refreshed HTML Structure, Avoiding Duplication of Config

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-createAutoComplete({
-  root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster;
     return `
@@ -27,6 +26,15 @@ createAutoComplete({
 
     return response.data.Search;
   }
+}
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete')
+});
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete')
 });
 
 
